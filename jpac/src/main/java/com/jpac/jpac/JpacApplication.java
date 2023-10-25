7f97abcf9a862e696a5693ae144a76c989c9ceb2
Balajijebbu <jeyabalajichandrasekaran@gmail.com>
2023-10-25 20:04:28 +0530

    first commit

---
+++ jpac/src/main/java/com/jpac/jpac/JpacApplication.java
@@ -0,0 +1,9 @@
+package com.jpac.jpac;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class JpacApplication {
+public static void main(String[] args) {
+SpringApplication.run(JpacApplication.class, args);
+}
+}
---
+++ jpac/src/main/java/com/jpac/jpac/Repo.java
@@ -0,0 +1,4 @@
+package com.jpac.jpac;
+import org.springframework.data.repository.CrudRepository;
+public interface Repo extends CrudRepository<User,Integer>{
+}
